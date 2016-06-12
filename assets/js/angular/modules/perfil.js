ad51a907be21da75b2837ee5b67bcb777f65a901
Victor <victor.munidario@gmail.com>
2016-06-12 02:32:41 -0300

    Implementacao do metodo get_tweets funcionando

--- assets/js/angular/modules/perfil.js
+++ assets/js/angular/modules/perfil.js
@@ -24,37 +24,14 @@ perfil_module.controller('perfilController', function ($scope, Service) {
 	$scope.user_birthday = user.birthday;
 	$scope.user_email = user.email;
 
+	$scope.tweets = [];
+
 	//Recupera posts do usuário
 	Service.get_tweets(user.id).then(
 		//Sucesso
 		function (res) {
 			// Array de tweets
-			var tweets = res.data;
-
-			// Para cada tweet do usuario, adiciona uma div no html
-			for(var tweet in tweets) {
-				var div_tweet_list = '<div class="row">'+
-							'<div id="posts" class="panel panel-default">'+
-								'<div class="panel-heading">'+
-									'<b>'+tweet.title+'</b>'+
-								'</div>'+
-								'<div class="panel-body">'+
-									'<div class="col-sm-2">'+
-										'<img ng-src="'+$scope.user_photo+'">'+
-									'</div>'+
-									'<div class="col-sm-9">'+
-										'<p>'+tweet.text+'</p>'+
-										'<p><small>'+$scope.user_name+'</small></p>'+
-									'</div>'+
-									'<div class="col-sm-1">'+
-										'<button ng-click="editPost()" type="button" class="btn btn-default" aria-label="Left Align">'+
-				  							'<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>'+
-										'</button>'+
-									'</div>'+
-								'</div>'+
-							'</div>';
-				$('#div-tweet-list').append(div_tweet_list);
-			}
+			$scope.tweets = res.data;
 		},
 		//Erro
 		function (res) {

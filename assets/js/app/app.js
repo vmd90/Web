f7f5e68b3f5de0888fa5d306f1cbe55d0b678c32
Victor Municelli Dario <victor.munidario@gmail.com>
2017-07-03 21:21:55 -0300

    Erasing # in path, signup

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,39 +1,38 @@
-
 (function() {
     "use strict";
 
-	angular.module('app', [
-		'ngRoute'
-	])
-	.config(['$routeProvider', function($routeProvider){
-		$routeProvider
-			.when('/', {
-				templateUrl: '/templates/login.html',
-				controller: 'loginController'
-			}).when('/index', {
-				templateUrl: '/templates/index.html',
-				controller: 'indexController'
-			}).when('/cadastro', {
-				templateUrl: '/templates/cadastro.html',
-				controller: ''
-			}).when('/perfil', {
-				templateUrl: '/templates/perfil.html',
-				controller: 'perfilController'
-			}).when('/perfil-editar', {
-				templateUrl: '/templates/perfil-editar.html',
-				controller: 'perfilEditarController'
-			}).when('/grupos', {
-				templateUrl: '/templates/grupos.html',
-				controller: 'gruposController'
-			}).when('/grupo', {
-				templateUrl: '/templates/grupo.html',
-				controller: 'grupoController'
-			}).when('/relatorios', {
-				templateUrl: '/templates/relatorios.html',
-				controller: 'relatoriosController'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-	}]);
+    angular
+        .module('app', [
+            'ngRoute'
+        ])
+        .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: '/templates/login.html',
+                    controller: 'loginController'
+                }).when('/index', {
+                    templateUrl: '/templates/index.html',
+                    controller: 'indexController'
+                }).when('/signup', {
+                    templateUrl: '/templates/signup.html',
+                    controller: ''
+                }).when('/perfil', {
+                    templateUrl: '/templates/perfil.html',
+                    controller: 'perfilController'
+                }).when('/perfil-editar', {
+                    templateUrl: '/templates/perfil-editar.html',
+                    controller: 'perfilEditarController'
+                }).when('/grupos', {
+                    templateUrl: '/templates/grupos.html',
+                    controller: 'gruposController'
+                }).when('/grupo', {
+                    templateUrl: '/templates/grupo.html',
+                    controller: 'grupoController'
+                }).when('/relatorios', {
+                    templateUrl: '/templates/relatorios.html',
+                    controller: 'relatoriosController'
+                });
+
+            $locationProvider.html5Mode(true);
+        }]);
 })();
--- assets/js/app/modules/login.js
+++ assets/js/app/modules/login.js
@@ -1,40 +1,37 @@
-
 (function() {
     "use strict";
 
-	angular.module('app')
-	.controller('loginController', function ($scope, Service, $location) {
+    angular
+        .module('app')
+        .controller('loginController', function($scope, Service, $location) {
 
-		$scope.login = function() {
-			var email = $scope.email;
-			var password = $scope.password;
-			//var email = 'vitor@usp.br';
-			//var password = 'vitor';
+            $scope.login = function() {
+                    var email = $scope.email;
+                    var password = $scope.password;
 
+                    Service.login(email, password).then(
+                        //Sucesso
+                        function(res) {
+                            if (res.data) {
+                                //console.log(res.data.id);
+                                Service.set_user(res.data);
+                                //window.location = "#/index.html";
+                                $location.path('/index');
+                            } else {
+                                alert('Email ou Password invalidos !');
+                            }
+                        },
 
-			Service.login(email, password).then(
-				//Sucesso
-				function (res) {
-					if(res.data){
-						//console.log(res.data.id);
-						Service.set_user(res.data);
-						//window.location = "#/index.html";
-						$location.path('/index');
-					}else{
-						alert('Email ou Password invalidos !');
-					}
-				},
+                        //Erro
+                        function(res) {
+                            alert('Erro ao consultar banco !');
+                        }
+                    );
+                },
 
-				//Erro
-				function (res) {
-					alert('Erro ao consultar banco !');
-				}
-			);
-		},
-
-		$scope.cadastrar = function(){
-			//window.location = "#/cadastro.html";
-			$location.path('/cadastro');
-		}
-	});
+                $scope.signup = function() {
+                    //window.location = "#/cadastro.html";
+                    $location.path('/signup');
+                }
+        });
 })();
